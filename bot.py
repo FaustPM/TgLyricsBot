0a978889ea2420a6ab619e142a14086971ab7b7e
Никита Голицын <102051531+FaustPM@users.noreply.github.com>
2025-01-24 13:50:34 +0300

    tg_bot v0.5

--- bot.py
+++ bot.py
@@ -1,45 +1,58 @@
 import logging
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
-from transformers import pipeline
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import lyricsgenius
+import asyncio
+from requests.exceptions import ReadTimeout
+
+# Вставьте ваш токен Telegram-бота
+TELEGRAM_TOKEN = 'Ваш ключ'
+
+# Вставьте ваш токен доступа Genius
+GENIUS_ACCESS_TOKEN = 'Ваш ключ'
 
 # Настройка логирования
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Загрузка модели для генерации текста
-chatbot = pipeline("text-generation", model="sberbank-ai/rugpt3small_based_on_gpt2")
+# Инициализация клиента Genius
+genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
 
-# Обработчик команды /start
-async def start(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Привет! Я твой чат-бот. Напиши мне что-нибудь, и я постараюсь ответить.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf'Привет, {user.mention_html()}! Отправь мне название песни, и я пришлю тебе её текст.',
+    )
 
-# Обработчик текстовых сообщений
-async def echo(update: Update, context: CallbackContext) -> None:
+async def get_lyrics(song_title: str) -> str:
+    try:
+        song = genius.search_song(song_title)
+        if song:
+            return song.lyrics
+        else:
+            return None
+    except ReadTimeout as e:
+        logger.error(f"Ошибка при поиске песни '{song_title}': {e}")
+        return "Произошла ошибка при получении текста песни из-за проблем с подключением. Пожалуйста, попробуйте позже."
+    except Exception as e:
+        logger.error(f"Ошибка при поиске песни '{song_title}': {e}")
+        return "Произошла ошибка при получении текста песни."
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_message = update.message.text
-    logger.info(f"User: {user_message}")
+    lyrics = await get_lyrics(user_message)
+    if lyrics:
+        await update.message.reply_text(lyrics)
+    else:
+        await update.message.reply_text('Произошла ошибка при получении текста песни.')
 
-    # Генерация ответа с помощью модели
-    bot_response = chatbot(user_message, max_length=50, num_return_sequences=1)[0]['generated_text']
-    logger.info(f"Bot: {bot_response}")
-
-    await update.message.reply_text(bot_response)
-
-# Обработчик ошибок
-async def error(update: Update, context: CallbackContext) -> None:
-    logger.warning(f'Update {update} caused error {context.error}')
-
-# Основная функция
 def main() -> None:
-    # Вставь сюда свой токен
-    application = Application.builder().token("7083802706:AAEettPYqUtyaWQCzMzP6rtJZhyej4MGaVs").build()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # Регистрация обработчиков
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.add_error_handler(error)
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    # Запуск бота
     application.run_polling()
 
 if __name__ == '__main__':
